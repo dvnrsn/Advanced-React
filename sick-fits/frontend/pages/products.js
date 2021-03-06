dfd01b7b90fec8775a669e4bfa252de0b6c5fe29
Devin Rasmussen <devin.rasmussen@getcanopy.com>
2021-03-05 18:48:42 -0700

    all changes

---
+++ sick-fits/frontend/pages/products.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import Products from '../components/Products';
+
+export default function ProductsPage(props) {
+  return (
+    <div>
+      <Products />
+    </div>
+  );
+}
